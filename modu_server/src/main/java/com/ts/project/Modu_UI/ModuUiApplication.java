ef200bd6fb930bca81ec50f67a2cdba042be0b1f
recordbuffer <76642597+recordbuffer@users.noreply.github.com>
2022-06-22 09:57:33 +0900

    Backend setup

---
+++ modu_server/src/main/java/com/ts/project/Modu_UI/ModuUiApplication.java
@@ -0,0 +1,13 @@
+package com.ts.project.Modu_UI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuUiApplication.class, args);
+	}
+
+}
---
+++ modu_server/src/test/java/com/ts/project/Modu_UI/ModuUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts.project.Modu_UI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
